c4b1fb0760cd417230641892f153f132c209a2a7
Ivan-Casales <101676371+Ivan-Casales@users.noreply.github.com>
2025-05-15 15:54:05 -0300

    feat: api project creation

---
+++ source/back-end/app/core/config.py
@@ -0,0 +1,11 @@
+from pydantic import BaseSettings
+
+class Settings(BaseSettings):
+    WATSONX_URL: str
+    WATSONX_PROJECT_ID: str
+    WATSONX_APIKEY: str
+
+    class Config:
+        env_file = ".env"
+
+settings = Settings()
